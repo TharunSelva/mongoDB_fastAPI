28133e4f9204499c038344cd04724ff4418d94bd
TharunSelva <117023666+TharunSelva@users.noreply.github.com>
2025-07-28 16:43:03 +0100

    Delete MongoDB directory

--- MongoDB/MongoDB with FastAPI project/config/database.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://tharunselvats:5azMDrDgxTxI0XMp@crunchynutcluster.vwb407j.mongodb.net/")
-
-db = client.todo_db
-collection_name = db["todo_collection"]
--- MongoDB/schemas.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Dict, Any
-
-class AggregateRequest(BaseModel):
-    pipeline: List[Dict[str, Any]]
