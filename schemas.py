58270a40e8fa50d12396af9ef6989aa244dac64d
TharunSelva <117023666+TharunSelva@users.noreply.github.com>
2025-07-30 14:47:55 +0100

    added this to specify that any collection can be used (previously only the "comments" collection):

--- schemas.py
+++ schemas.py
@@ -2,4 +2,6 @@ from pydantic import BaseModel
 from typing import List, Dict, Any
 
 class AggregateRequest(BaseModel):
+    # added this to specify that any collection can be used (previously only the "comments" collection):
+    collection: str
     pipeline: List[Dict[str, Any]]
