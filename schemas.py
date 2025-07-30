cdfbae75bdc338682b1683d386e0398a92b3925b
TharunSelva <117023666+TharunSelva@users.noreply.github.com>
2025-07-30 16:16:04 +0100

    Fixing parsing issue - Input should be a valid dictionary
    Error:
    * What movies were released in the year 1909
    
    i'm getting this issue:
    
    Render (api hosting tool) log:
    INFO:     18.191.79.42:0 - "POST /aggregate?= HTTP/1.1" 422 Unprocessable Content
    
    from the agentic ai platform that i'm using:
    {'error_code': 'TOOL_FAILURE', 'message': '{"detail":[{"type":"dict_type","loc":["body","pipeline",0],"msg":"Input should be a valid dictionary","input":"{ \\"$match\\": { \\"release_year\\": 1909 } }"}]}', 'status_code': 500}
    
    the query input:
    {"body": {"form-data": {}, "json": {"collection": "movies", "pipeline": ["{ \"$match\": { \"release_year\": 1909 } }"]}}}

--- schemas.py
+++ schemas.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
+"""from pydantic import BaseModel
 from typing import List, Dict, Any
 
 class AggregateRequest(BaseModel):
     # added this to specify that any collection can be used (previously only the "comments" collection):
     collection: str
     pipeline: List[Dict[str, Any]]
+"""
+
+from pydantic import BaseModel
+from typing import List, Dict, Any, Union
+
+class AggregateRequest(BaseModel):
+    collection: str
+    pipeline: Union[List[Dict[str, Any]], List[str]]
