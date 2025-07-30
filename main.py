dc7f72cde0cafa0d3301744865872cda7bdbdbdf
TharunSelva <117023666+TharunSelva@users.noreply.github.com>
2025-07-30 14:59:48 +0100

    Works for all collections

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 from typing import List, Dict, Any
 from db import db
+from dotenv import load_dotenv
+from schemas import AggregateRequest
+from bson import ObjectId
 import os
-from bson import ObjectId  # Needed to handle MongoDB ObjectId
+
+# Load environment variables
+load_dotenv()
 
 app = FastAPI()
 
 API_KEY = os.getenv("API_KEY")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+if not API_KEY:
+    raise RuntimeError("API_KEY not set in .env")
 
 api_key_header = APIKeyHeader(name="X-API-Key")
 
@@ -17,7 +23,7 @@ def verify_api_key(api_key: str = Depends(api_key_header)):
     if api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
-# Helper to convert ObjectId to string
+# Helper function to convert ObjectId to string
 def convert_objectids(obj):
     if isinstance(obj, list):
         return [convert_objectids(item) for item in obj]
@@ -28,26 +34,22 @@ def convert_objectids(obj):
     else:
         return obj
 
-# Pydantic model for request body
-class AggregateRequest(BaseModel):
-    pipeline: List[Dict[str, Any]]
-
 @app.post("/aggregate")
 async def aggregate_query(
     payload: AggregateRequest,
     api_key: str = Depends(verify_api_key)
 ):
     pipeline = payload.pipeline
+    collection_name = payload.collection
 
     if not isinstance(pipeline, list):
         raise HTTPException(status_code=400, detail="Pipeline must be a list")
 
-    collection = db[COLLECTION_NAME]
     try:
+        collection = db[collection_name]
         cursor = collection.aggregate(pipeline)
         results = await cursor.to_list(length=1000)
         results = convert_objectids(results)
+        return {"results": results}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-    return {"results": results}
